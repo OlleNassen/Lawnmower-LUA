3810a6c2ec984cdad02bbb7638b6bc5eecdddf77
Fredholm <te11c.simon.fredholm@gmail.com>
2017-04-27 14:15:48 +0200

    another fix on script

--- Lawnmower LUA/Scripts/player.lua
+++ Lawnmower LUA/Scripts/player.lua
@@ -9,44 +9,36 @@ end
 --mx = map x max, my = map y max
 
 function collision(mx, my)
-	if (position.x > mx) then
-position.x = mx
-	end
-
-	elseif (position.x < 0) then
- position.x = 0
-	end
-
-	if (position.y > my) then
-position.y = my
-	end
-
-	elseif (position.y < 0) then
- position.y = 0
+	if position.x > mx then
+        position.x = mx
+	elseif position.x < 0 then
+        position.x = 0
+    end
+	
+    if position.y > my then
+        position.y = my
+	elseif position.y < 0 then
+        position.y = 0
 	end
 
 end
 
 function move(delta, verticalDir, horizontalDir)
 
-if (horizontalDir == “left”) then
-	position.x -= delta * speed
-end
+    if horizontalDir == “left”) then
+        position.x -= delta * speed
+    elseif horizontalDir == “right” then
+        position.x += delta * speed
+    end
 
-elseif (horizontalDir == “right”) then
-	position.x += delta * speed
-end
-
-if (verticalDir == “up”) then
-	position.y -= delta * speed
-end
-
-elseif (verticalDir == “down”) then
-	position.y += delta * speed
-end
+    if verticalDir == “up” then
+        position.y -= delta * speed
+    elseif verticalDir == “down” then
+        position.y += delta * speed
+    end
 
 end
 
 function getPosition()
-return position.x, position.y	
+    return position.x, position.y	
 end
