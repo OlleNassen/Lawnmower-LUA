59a098b904e7518d014b205a536b34f9941289bc
Fredholm <te11c.simon.fredholm@gmail.com>
2017-04-27 16:16:48 +0200

    boom

---
+++ Lawnmower LUA/Scripts/tile.lua
@@ -0,0 +1,17 @@
+﻿position = {x, y}
+dimensions = { 32,32 } -- size
+type = name
+
+function setPosition(x, y)
+	position.x = x;
+	position.y = y;
+end
+
+function getPosition()
+	return position.x, position.y    
+end
+
+function changeType(inType)
+	type = inType
+ChangeTexture(type)  
+end
