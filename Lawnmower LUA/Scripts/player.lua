8b04a24dffc894c5d63fad84086f830f2daa33ed
Fredholm <te11c.simon.fredholm@gmail.com>
2017-04-27 14:10:05 +0200

    added our player.lua

---
+++ Lawnmower LUA/Scripts/player.lua
@@ -0,0 +1,52 @@
+position = {x = 0, y = 0}
+speed = 3
+
+function setPosition(x, y)
+	position.x = x
+	position.y = y
+end
+
+--mx = map x max, my = map y max
+
+function collision(mx, my)
+	if (position.x > mx) then
+position.x = mx
+	end
+
+	else if (position.x < 0) then
+ position.x = 0
+	end
+
+	if (position.y > my) then
+position.y = my
+	end
+
+	else if (position.y < 0) then
+ position.y = 0
+	end
+
+end
+
+function move(delta, verticalDir, horizontalDir)
+
+if (horizontalDir == “left”) then
+	position.x -= delta * speed
+end
+
+else if (horizontalDir == “right”) then
+	position.x += delta * speed
+end
+
+if (verticalDir == “up”) then
+	position.y -= delta * speed
+end
+
+else if (verticalDir == “down”)
+	position.y += delta * speed
+end
+
+end
+
+function getPosition()
+return position.x, position.y	
+end
