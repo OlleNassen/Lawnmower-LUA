81e42a74a838067f8f9efe73fe870c003f27add7
bjurrsson <emanuel.bjurman@gmail.com>
2017-04-27 13:27:38 +0200

    More Tile

--- Lawnmower LUA/Tile.cpp
+++ Lawnmower LUA/Tile.cpp
@@ -1,12 +1,31 @@
 #include "Tile.h"
 
-Tile::Tile()
+Tile::Tile() { }
+
+Tile::Tile(std::string texturePath)
+{
+	loadTexture(texturePath);
+}
+
+Tile::Tile(const Tile & other) { }
+
+Tile::~Tile() { }
+
+bool Tile::loadTexture(std::string path)
+{
+	if (!m_texture.loadFromFile(path))
+		return false;
+
+	m_sprite.setTexture(m_texture);
+	return true;
+}
+
+void Tile::update()
 {
 
 }
 
-Tile::~Tile()
+void Tile::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-
+	target.draw(m_sprite, states);
 }
-
--- Lawnmower LUA/Tile.h
+++ Lawnmower LUA/Tile.h
@@ -1,14 +1,23 @@
 #ifndef TILE_H
 #define TILE_H
 #include <SFML\Graphics.hpp>
+#include <string>
 
 class Tile : public sf::Drawable
 {
 public:
 	Tile();
+	Tile(std::string texturePath);
+	Tile(const Tile& other);
 	~Tile();
+
+	bool loadTexture(std::string path);
+	void update();
 private:
-	
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+	sf::Texture m_texture;
+	sf::Sprite m_sprite;
 };
 
 #endif
