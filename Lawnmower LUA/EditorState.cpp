51c2b71ffcc88bbb8fe85747bfd5919cb0cb1795
Fredholm <te11c.simon.fredholm@gmail.com>
2017-05-04 15:37:07 +0200

    asdf

--- Lawnmower LUA/EditorState.cpp
+++ Lawnmower LUA/EditorState.cpp
@@ -6,6 +6,11 @@ EditorState::EditorState(sf::RenderWindow& window, std::shared_ptr<ResourceManag
     :State(window)
 {
     m_resources = resources;
+
+	// Initialize Lua
+	L = luaL_newstate();
+	luaL_openlibs(L);
+	loadLuaScript();
 }
 
 
@@ -49,4 +54,14 @@ void EditorState::pause()
 
 void EditorState::resume()
 {
+}
+
+void EditorState::loadLuaScript()
+{
+	int error = luaL_dofile(L, "Scripts/editor.lua");
+	if (error == 1)
+	{
+		std::cout << "Failed to load with message: " << lua_tostring(L, -1) << std::endl;
+		lua_pop(L, 1);
+	}
 }
